8ce54cd3fb2bc9d6116e9a69bf45fbfda594ffba
calibur <caliburex@163.com>
2020-04-13 17:03:27 +0800

    chore(all): 项目初始化🎉

---
+++ React/components/Cascarder.js
@@ -0,0 +1,36 @@
+/**
+ * @file Cascarder Component
+ * @author calibur
+ * @time 2019/11/06
+ */
+
+import React, { Component, PureComponent } from 'react';
+import propTypes from 'prop-types';
+
+/**
+ * @desc Cascarder Class
+ * @param {object} props
+ * @param {array} props.options - 所有选项
+ * @tips 这个组件被设计成了完全非受控组件
+ *			 如果有需要更改options,value等
+ *			 那你需要重新实例化这个组件(唯一key)
+ * @return ReactComponent
+*/
+class Cascarder extends Component {
+	
+	consturctor(props) {
+			
+	}
+
+	static getDerivedStateFromProps(nextProps, preState) {
+
+	}
+
+	componentWillReceiveProps(nextProps) {
+			
+	}
+
+	shouldComponentUpdate() {
+	
+	}
+}
